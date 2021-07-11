b07938b650f7eb4af62313c3b5deff569e14f3ee
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-07-11 14:32:34 -0400

    added Updating State That Depends On The Previous State

--- src/components/NewExpense/ExpenseForm/ExpenseForm.Componenet.jsx
+++ src/components/NewExpense/ExpenseForm/ExpenseForm.Componenet.jsx
@@ -20,21 +20,33 @@ const ExpenseForm = () => {
     */
 
     const titleChangeHandler = event => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
+        // setUserInput({
+        //     ...userInput, // userInput is being decoupled here
+        //     enteredTitle: event.target.value // enteredTitle is set to event.target.value
+        // })
+
+        setUserInput((prevState)=>{
+            return {...prevState, enteredTitle:event.target.value}
         })
     }
     const amountChangeHandler = event => {
-        setUserInput({
-            ...userInput,
-            enteredAmount:event.target.value
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount:event.target.value
+        // })
+
+        setUserInput((prevState)=>{
+            return {...prevState, enteredAmount:event.target.value}
         })
     }
     const dateChangeHandler = event => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // })
+
+        setUserInput((prevState)=>{
+            return {...prevState, enteredDate:event.target.value}
         })
     }
     return (
